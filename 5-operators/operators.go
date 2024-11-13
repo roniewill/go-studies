5cd7bd0c871f39985d7fc0c0e6febad3ee67fea0
Ramon William <roniewill@gmail.com>
2024-11-13 09:07:17 -0300

    Learning what operators are and how to them

---
+++ 5-operators/operators.go
@@ -0,0 +1,54 @@
+package main
+
+func main() {
+	// ## Arithmetics operators
+	// plus := 1 + 3
+	// minos := 5 - 2
+	// multiple := 2 * 5
+	// divide := 10 / 5
+	// restOfDivide := 10 % 3
+
+	// fmt.Println(plus, minos, multiple, divide, restOfDivide)
+
+	// ## Assignment operators
+	// var var01 string = "String one"
+	// var02 := "String two"
+	// var03 := var01
+	// fmt.Println(var01, var02, var03)
+
+	// ## Relational operators
+	// fmt.Println(1 > 2)
+	// fmt.Println(1 < 2)
+	// fmt.Println(1 == 2)
+	// fmt.Println(1 <= 2)
+	// fmt.Println(1 >= 2)
+	// fmt.Println(1 != 2)
+
+	// ## Logical operators
+	// result01, result02 := 1 > 2, 5 == 5
+	// fmt.Println(result01 && result02)
+	// fmt.Println(result01 || result02)
+	// fmt.Println(!result01)
+	// fmt.Println(!result02)
+
+	// ## Unary operators
+	// number := 10
+	// fmt.Println(number)
+	// number++
+	// fmt.Println(number)
+	// number += 15
+	// fmt.Println(number)
+	// number--
+	// fmt.Println(number)
+	// number -= 12
+	// fmt.Println(number)
+	// number *= 3
+	// fmt.Println(number)
+	// number /= 9
+	// fmt.Println(number)
+	// number %= 2
+	// fmt.Println(number)
+
+	// ## ternary operators
+	// Ternary operators don't exist in Golang, so you'll have to use the good old if statement :)
+}
